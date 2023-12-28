652af86c2ca97a7d698a2744c375d17b21107ad7
harsh0703-harsh <harsharora0703@gmail.com>
2023-12-28 15:38:21 +0530

    get providers api

--- app.ts
+++ app.ts
@@ -1,10 +1,38 @@
 import express from 'express';
 
 import authRouters from './routes/auth.route.js';
+import connectToMongoDB from './schema/connection.js';
+import { Client } from './schema/client.schema.js';
 
 const app = express();
+connectToMongoDB()
+
 
 app.use('/auth',authRouters);
+
+app.get("/providers" , async (req, res)=>{
+
+    const providers = await Client.find({});
+
+    if(providers.length){
+
+
+        res.status(200).json(
+            {
+                clients : providers
+            }
+        )
+
+    }else{
+
+        res.status(400).json({
+
+            error : "No providers found"
+
+        })
+    }
+
+})
 
 app.listen(4000,()=>{
 
